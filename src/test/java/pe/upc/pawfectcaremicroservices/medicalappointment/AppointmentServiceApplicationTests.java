26951724de6534ac9f10f29b613401f767ebb514
Jose Gutierrez <josecitogutigar21@gmail.com>
2025-07-12 04:04:50 -0500

    feat: add appointment service

---
+++ src/test/java/pe/upc/pawfectcaremicroservices/medicalappointment/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upc.pawfectcaremicroservices.medicalappointment;
+
+import org.junit.jupiter.api.Test;
+
+class AppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Test básico que siempre pasa
+		System.out.println("Test ejecutado correctamente.");
+	}
+
+}
